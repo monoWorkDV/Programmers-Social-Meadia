531d45e37f8aab13fa4b4e95141afd0e0d1f77ec
MohammadReza <basket.mono.7@gmail.com>
2024-12-27 02:03:13 +0330

    user 109;lastname changed

--- js/script.js
+++ js/script.js
@@ -1,165 +1,165 @@
 //! states
 let users = [
-  {
-    id: 101,
-    firstName: "علیرضا",
-    lastName: "محمدی کردخیلی",
-    image: "images/profile-0.jpg",
-    job: "توسعه دهنده وب",
-    following: 272,
-    follower: 2000,
-    projects: 9,
-    salary: 120000000,
-    workHours: 49,
-  },
-  {
-    id: 102,
-    firstName: "نوید",
-    lastName: "نیکنام",
-    image: "images/profile-1.jpg",
-    job: "برنامه‌نویس ارشد بک‌اند",
-    following: 272,
-    follower: 1200,
-    projects: 60,
-    salary: 170000000,
-    workHours: 50,
-  },
-  {
-    id: 103,
-    firstName: "روژین",
-    lastName: "کریمی",
-    image: "images/profile-8w.jpg",
-    job: "برنامه‌نویس وب",
-    following: 50,
-    follower: 350,
-    projects: 55,
-    salary: 220000000,
-    workHours: 42,
-  },
-  {
-    id: 104,
-    firstName: "نازلی",
-    lastName: "موسوی",
-    image: "images/profile-5w.jpg",
-    job: "مهندس نرم افزار",
-    following: 1002,
-    follower: 20,
-    projects: 9,
-    salary: 120000000,
-    workHours: 49,
-  },
-  {
-    id: 105,
-    firstName: "هادی",
-    lastName: "کریمی",
-    image: "images/profile-4.jpg",
-    job: "کارشناس سئو",
-    following: 2720,
-    follower: 7050,
-    projects: 0,
-    salary: 185000000,
-    workHours: 49,
-  },
-  {
-    id: 106,
-    firstName: "پوریا",
-    lastName: "براتیان",
-    image: "images/profile-3.jpg",
-    job: "کارشناس تولید محتوا",
-    following: 9872,
-    follower: 101,
-    projects: 1,
-    salary: 280000000,
-    workHours: 56,
-  },
-  {
-    id: 107,
-    firstName: "ایمان",
-    lastName: "جلیلی",
-    image: "images/profile-6.jpg",
-    job: "گرافیست",
-    following: 65,
-    follower: 2000,
-    projects: 89,
-    salary: 155000000,
-    workHours: 30,
-  },
-  {
-    id: 108,
-    firstName: "پارسا",
-    lastName: "سلمانیان",
-    image: "images/profile-7.jpg",
-    job: "عکاسی و تولید محتوا",
-    following: 21,
-    follower: 2000,
-    projects: 20,
-    salary: 81000000,
-    workHours: 55,
-  },
-  {
-    id: 109,
-    firstName: "سهیل",
-    lastName: "نعمتی",
-    image: "images/profile-2.jpg",
-    job: "سرپرست فروش",
-    following: 50,
-    follower: 2000,
-    projects: 89,
-    salary: 90000000,
-    workHours: 44,
-  },
-  {
-    id: 110,
-    firstName: "مریم",
-    lastName: "علوی",
-    image: "images/profile-9w.jpg",
-    job: "تصویر بردار و تدوین‌گر",
-    following: 272,
-    follower: 2000,
-    projects: 50,
-    salary: 450000000,
-    workHours: 46,
-  },
-  {
-    id: 111,
-    firstName: "علی",
-    lastName: "سلطانیان",
-    image: "images/profile-10.jpg",
-    job: "مدیر خلاقیت",
-    following: 272,
-    follower: 2000,
-    projects: 32,
-    salary: 340000000,
-    workHours: 54,
-  },
+   {
+      id: 101,
+      firstName: "علیرضا",
+      lastName: "محمدی کردخیلی",
+      image: "images/profile-0.jpg",
+      job: "توسعه دهنده وب",
+      following: 272,
+      follower: 2000,
+      projects: 9,
+      salary: 120000000,
+      workHours: 49,
+   },
+   {
+      id: 102,
+      firstName: "نوید",
+      lastName: "نیکنام",
+      image: "images/profile-1.jpg",
+      job: "برنامه‌نویس ارشد بک‌اند",
+      following: 272,
+      follower: 1200,
+      projects: 60,
+      salary: 170000000,
+      workHours: 50,
+   },
+   {
+      id: 103,
+      firstName: "روژین",
+      lastName: "کریمی",
+      image: "images/profile-8w.jpg",
+      job: "برنامه‌نویس وب",
+      following: 50,
+      follower: 350,
+      projects: 55,
+      salary: 220000000,
+      workHours: 42,
+   },
+   {
+      id: 104,
+      firstName: "نازلی",
+      lastName: "موسوی",
+      image: "images/profile-5w.jpg",
+      job: "مهندس نرم افزار",
+      following: 1002,
+      follower: 20,
+      projects: 9,
+      salary: 120000000,
+      workHours: 49,
+   },
+   {
+      id: 105,
+      firstName: "هادی",
+      lastName: "کریمی",
+      image: "images/profile-4.jpg",
+      job: "کارشناس سئو",
+      following: 2720,
+      follower: 7050,
+      projects: 0,
+      salary: 185000000,
+      workHours: 49,
+   },
+   {
+      id: 106,
+      firstName: "پوریا",
+      lastName: "براتیان",
+      image: "images/profile-3.jpg",
+      job: "کارشناس تولید محتوا",
+      following: 9872,
+      follower: 101,
+      projects: 1,
+      salary: 280000000,
+      workHours: 56,
+   },
+   {
+      id: 107,
+      firstName: "ایمان",
+      lastName: "جلیلی",
+      image: "images/profile-6.jpg",
+      job: "گرافیست",
+      following: 65,
+      follower: 2000,
+      projects: 89,
+      salary: 155000000,
+      workHours: 30,
+   },
+   {
+      id: 108,
+      firstName: "پارسا",
+      lastName: "سلمانیان",
+      image: "images/profile-7.jpg",
+      job: "عکاسی و تولید محتوا",
+      following: 21,
+      follower: 2000,
+      projects: 20,
+      salary: 81000000,
+      workHours: 55,
+   },
+   {
+      id: 109,
+      firstName: "سهیل",
+      lastName: "علیپور",
+      image: "images/profile-2.jpg",
+      job: "سرپرست فروش",
+      following: 50,
+      follower: 2000,
+      projects: 89,
+      salary: 90000000,
+      workHours: 44,
+   },
+   {
+      id: 110,
+      firstName: "مریم",
+      lastName: "علوی",
+      image: "images/profile-9w.jpg",
+      job: "تصویر بردار و تدوین‌گر",
+      following: 272,
+      follower: 2000,
+      projects: 50,
+      salary: 450000000,
+      workHours: 46,
+   },
+   {
+      id: 111,
+      firstName: "علی",
+      lastName: "سلطانیان",
+      image: "images/profile-10.jpg",
+      job: "مدیر خلاقیت",
+      following: 272,
+      follower: 2000,
+      projects: 32,
+      salary: 340000000,
+      workHours: 54,
+   },
 ];
 let topUsers = {
-  bySalary: -1,
-  byProjects: -1,
+   bySalary: -1,
+   byProjects: -1,
 };
 let gradients = [
-  "from-red-500 to-orange-500",
-  "from-orange-500 to-amber-500",
-  "from-amber-500 to-yellow-500",
-  "from-yellow-500 to-lime-500",
-  "from-lime-500 to-green-500",
-  "from-green-500 to-emerald-500",
-  "from-emerald-500 to-teal-500",
-  "from-teal-500 to-cyan-500",
-  "from-cyan-500 to-sky-500",
-  "from-sky-500 to-blue-500",
-  "from-blue-500 to-indigo-500",
-  "from-indigo-500 to-violet-500",
-  "from-violet-500 to-purple-500",
-  "from-purple-500 to-fuchsia-500",
-  "from-fuchsia-500 to-pink-500",
-  "from-pink-500 to-rose-500",
+   "from-red-500 to-orange-500",
+   "from-orange-500 to-amber-500",
+   "from-amber-500 to-yellow-500",
+   "from-yellow-500 to-lime-500",
+   "from-lime-500 to-green-500",
+   "from-green-500 to-emerald-500",
+   "from-emerald-500 to-teal-500",
+   "from-teal-500 to-cyan-500",
+   "from-cyan-500 to-sky-500",
+   "from-sky-500 to-blue-500",
+   "from-blue-500 to-indigo-500",
+   "from-indigo-500 to-violet-500",
+   "from-violet-500 to-purple-500",
+   "from-purple-500 to-fuchsia-500",
+   "from-fuchsia-500 to-pink-500",
+   "from-pink-500 to-rose-500",
 ];
 let statistics = {
-  usersCount: seperator(users.length),
-  projectsCount: null,
-  averageSalary: null,
-  averageWorkHours: null,
+   usersCount: seperator(users.length),
+   projectsCount: null,
+   averageSalary: null,
+   averageWorkHours: null,
 };
 
 //! ELEMENTS
@@ -181,24 +181,23 @@ const modalImageEl = document.querySelector("#modal .user-card__image img");
 
 //! USER GRID
 function generateUsers(userArray) {
-  userGridEl.innerHTML = "";
-  userArray.forEach(function (user) {
-    const topSalaryLabel =
-      user.id === topUsers.bySalary
-        ? `<span class="dark:bg-gray-700 dark:text-white bg-gray-100 text-gray-800 text-xs font-medium px-2.5 py-0.5 rounded">
+   userGridEl.innerHTML = "";
+   userArray.forEach(function (user) {
+      const topSalaryLabel =
+         user.id === topUsers.bySalary
+            ? `<span class="dark:bg-gray-700 dark:text-white bg-gray-100 text-gray-800 text-xs font-medium px-2.5 py-0.5 rounded">
              بیشترین حقوق 
            </span>`
-        : ""; 
+            : "";
 
-
-    const topProjectLabel =
-      user.id === topUsers.byProjects
-        ? `<span class="dark:bg-gray-700 dark:text-white bg-gray-100 text-gray-800 text-xs font-medium px-2.5 py-0.5 rounded">
+      const topProjectLabel =
+         user.id === topUsers.byProjects
+            ? `<span class="dark:bg-gray-700 dark:text-white bg-gray-100 text-gray-800 text-xs font-medium px-2.5 py-0.5 rounded">
              بیشترین تعداد پروژه
            </span>`
-        : "";
+            : "";
 
-    const userHTML = `
+      const userHTML = `
         <div
           data-user-id="${user.id}"
           class="user-card w-full sm:w-1/2 lg:w-1/3 xl:w-1/4 text-center p-2"
@@ -206,7 +205,7 @@ function generateUsers(userArray) {
           <div class="user-card__inner border rounded-lg overflow-hidden cursor-pointer transition-all duration-500 hover:opacity-80">
             <div
               class="user-card__head ${
-                gradients[randomGenerator(0, gradients.length - 1)]
+                 gradients[randomGenerator(0, gradients.length - 1)]
               } flex flex-wrap items-start justify-end gap-2 p-2 relative -z-10 bg-gradient-to-r w-full h-20"
             >
               ${topSalaryLabel}${topProjectLabel}
@@ -244,8 +243,8 @@ function generateUsers(userArray) {
           </div>
         </div>
       `;
-    userGridEl.insertAdjacentHTML("beforeend", userHTML);
-  });
+      userGridEl.insertAdjacentHTML("beforeend", userHTML);
+   });
 }
 generateUsers(users);
 
@@ -255,55 +254,55 @@ topUsers.byProjects = customOrder("projects")[0].id;
 
 //! SORTING
 function customOrder(field) {
-  const usersCopy = [...users];
-  return usersCopy.sort((a, b) => b[field] - a[field]);
+   const usersCopy = [...users];
+   return usersCopy.sort((a, b) => b[field] - a[field]);
 }
 orderByEl.addEventListener("change", function (e) {
-  const field = e.target.value;
-  users = customOrder(field);
-  generateUsers(users);
-  modalHandler();
+   const field = e.target.value;
+   users = customOrder(field);
+   generateUsers(users);
+   modalHandler();
 });
 
 //! LIVE SEARCH
 function search(query) {
-  return users.filter(function (user) {
-    const fullName = `${user.firstName} ${user.lastName}`;
-    return fullName.includes(query) || user.job.includes(query);
-  });
+   return users.filter(function (user) {
+      const fullName = `${user.firstName} ${user.lastName}`;
+      return fullName.includes(query) || user.job.includes(query);
+   });
 }
 searchInputEl.addEventListener("keyup", function (e) {
-  const searchResultUsers = search(e.target.value);
-  generateUsers(searchResultUsers);
-  modalHandler();
+   const searchResultUsers = search(e.target.value);
+   generateUsers(searchResultUsers);
+   modalHandler();
 });
 searchInputEl.addEventListener("search", function (e) {
-  const searchResultUsers = search(e.target.value);
-  generateUsers(searchResultUsers);
-  modalHandler();
+   const searchResultUsers = search(e.target.value);
+   generateUsers(searchResultUsers);
+   modalHandler();
 });
 
 //! STATISTICS
 function setStatistics() {
-  let sum_projects = 0;
-  let sum_salary = 0;
-  let sum_workHours = 0;
-  users.forEach(function (user) {
-    sum_projects += user.projects;
-    sum_salary += user.salary;
-    sum_workHours += user.workHours;
-  });
-  statistics.projectsCount = seperator(sum_projects);
+   let sum_projects = 0;
+   let sum_salary = 0;
+   let sum_workHours = 0;
+   users.forEach(function (user) {
+      sum_projects += user.projects;
+      sum_salary += user.salary;
+      sum_workHours += user.workHours;
+   });
+   statistics.projectsCount = seperator(sum_projects);
 
-  //? averageSalary
-  const avgSalary = sum_salary / statistics.usersCount;
-  const avgSalaryRounded = Math.round(avgSalary / 1000000);
-  statistics.averageSalary = seperator(avgSalaryRounded);
+   //? averageSalary
+   const avgSalary = sum_salary / statistics.usersCount;
+   const avgSalaryRounded = Math.round(avgSalary / 1000000);
+   statistics.averageSalary = seperator(avgSalaryRounded);
 
-  //? averageWorkHours
-  const avgWorkHours = sum_workHours / statistics.usersCount;
-  const avgWorkHoursRounded = Math.round(avgWorkHours * 10) / 10;
-  statistics.averageWorkHours = seperator(avgWorkHoursRounded);
+   //? averageWorkHours
+   const avgWorkHours = sum_workHours / statistics.usersCount;
+   const avgWorkHoursRounded = Math.round(avgWorkHours * 10) / 10;
+   statistics.averageWorkHours = seperator(avgWorkHoursRounded);
 }
 setStatistics();
 
@@ -314,53 +313,49 @@ statisticWorkHoursEl.innerText = statistics.averageWorkHours + " ساعت";
 
 //! MODAL
 function modalHandler() {
-  const userCardsEls = document.querySelectorAll(".user-card");
-  userCardsEls.forEach((userCardEl) => {
-    userCardEl.addEventListener("click", function () {
-      const userId = +this.getAttribute("data-user-id");
-      const user = getUserById(userId);
-      updateModal(user);
-      showModal();
-    });
-  });
+   const userCardsEls = document.querySelectorAll(".user-card");
+   userCardsEls.forEach((userCardEl) => {
+      userCardEl.addEventListener("click", function () {
+         const userId = +this.getAttribute("data-user-id");
+         const user = getUserById(userId);
+         updateModal(user);
+         showModal();
+      });
+   });
 }
 modalHandler();
 
-
 function updateModal(userObj) {
-  modalFullnameEl.innerText = `${userObj.firstName} ${userObj.lastName}`;
-  modalJobEl.innerText = userObj.job;
-  modalSalaryEl.innerText = seperator(userObj.salary);
-  modalWorkHoursEl.innerText = userObj.workHours;
-  modalImageEl.setAttribute("src", userObj.image);
+   modalFullnameEl.innerText = `${userObj.firstName} ${userObj.lastName}`;
+   modalJobEl.innerText = userObj.job;
+   modalSalaryEl.innerText = seperator(userObj.salary);
+   modalWorkHoursEl.innerText = userObj.workHours;
+   modalImageEl.setAttribute("src", userObj.image);
 }
 
 closeBtnEl.addEventListener("click", hideModal);
 backdropEl.addEventListener("click", hideModal);
 
 function hideModal() {
-  backdropEl.classList.add("hidden");
-  modalEl.classList.add("hidden");
+   backdropEl.classList.add("hidden");
+   modalEl.classList.add("hidden");
 }
 
 function showModal() {
-  backdropEl.classList.remove("hidden");
-  modalEl.classList.remove("hidden");
+   backdropEl.classList.remove("hidden");
+   modalEl.classList.remove("hidden");
 }
 function getUserById(userId) {
-  const array = users.filter((user) => user.id === userId);
-  return array[0];
+   const array = users.filter((user) => user.id === userId);
+   return array[0];
 }
-
-
-
 
 //! HELPER FUNCTIONS (UTIL)
 function seperator(x) {
-  return Intl.NumberFormat("en-US").format(x);
+   return Intl.NumberFormat("en-US").format(x);
 }
 function randomGenerator(min, max) {
-  const random = min + Math.random() * (max - min);
-  const result = Math.round(random);
-  return result;
+   const random = min + Math.random() * (max - min);
+   const result = Math.round(random);
+   return result;
 }
